60fa681ebd0c08e70685f9ef5602491ef30e6d17
TehcJS <tehcdev@gmail.com>
2021-08-12 18:33:54 -0400

    tweak bloom

--- shaders/config/settings.glsl
+++ shaders/config/settings.glsl
@@ -1,5 +1,5 @@
 // Options
-#define BLOOM_INTENSITY 1.3 // [1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2]
+#define BLOOM_INTENSITY 2 // [1.5, 2, 2.5, 3]
 // #define DEBUG
 #define FLAT_NORMAL vec3(0, 1, 0) // The normal for flat "blocks" using the cross model
 // #define OLD_BLOOM // The old kernel for the bloom effect
