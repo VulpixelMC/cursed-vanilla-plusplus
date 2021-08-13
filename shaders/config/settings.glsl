d036a6ead16c9d43118a89ea1c8afd908e914f0c
TehcJS <tehcdev@gmail.com>
2021-08-13 10:09:19 -0400

    make emissive texture brightness less intense

--- shaders/config/settings.glsl
+++ shaders/config/settings.glsl
@@ -1,5 +1,5 @@
 // Options
-#define BLOOM_INTENSITY 2 // [1.5, 2, 2.5, 3]
+#define BLOOM_INTENSITY 2 // [1, 1.5, 2]
 // #define DEBUG
 #define FLAT_NORMAL vec3(0, 1, 0) // The normal for flat "blocks" using the cross model
 // #define OLD_BLOOM // The old kernel for the bloom effect
--- shaders/lib/bloom.glsl
+++ shaders/lib/bloom.glsl
@@ -56,7 +56,7 @@ BloomResult calcBloom(bool horizontal) {
 		float weight = weights[abs(i)];
 		vec2 dir = horizontal ? dirY(tex_offset.y, i) : dirX(tex_offset.x, i);
 
-		result += texture(colortex4, texcoord + dir) * weight * (BLOOM_INTENSITY * 0.5);
+		result += texture(colortex4, texcoord + dir) * weight * (BLOOM_INTENSITY * 0.75);
 		weightSum += weight;
 	}
 
